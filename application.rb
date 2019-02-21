614b155a169a550630f5d6553c39e1ff4852811f
Sim Greenbaum <gbs4ever2@gmail.com>
2019-02-21 23:01:39 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is "
+    resp.write "Hello, i am glad you are working . I seem to be disracted with QB questions anyway the point all knowledge is learned.  Do you know how many accounts google your questions ????"
     resp.finish
   end
 
